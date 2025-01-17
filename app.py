4a94e2248990a8b04a410e233e749a9a32579f63
Mohammad77Radwan <mohammadradwan804@gmail.com>
2025-01-17 12:45:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -37,21 +37,39 @@ st.write("Répondez aux questions ci-dessous pour découvrir votre affinité ave
 
 score = 0
 
-q1 = st.radio("1. Vous aimez :", ["Créer des sites web et des applications", "Configurer des réseaux", "Ni l'un ni l'autre"])
-if q1 == "Créer des sites web et des applications":
-    score += 1
+questions = [
+    ("1. Vous aimez :", ["Créer des sites web et des applications", "Configurer des réseaux", "Ni l'un ni l'autre"]),
+    ("2. Vous préférez :", ["Résoudre des problèmes de code", "Assembler des matériels", "Je ne sais pas"]),
+    ("3. Les bases de données vous semblent :", ["Fascinantes", "Compliquées", "Inintéressantes"]),
+    ("4. Vous aimez apprendre en :", ["Pratiquant avec des projets", "Écoutant des cours théoriques", "Je ne sais pas"]),
+    ("5. Vous êtes :", ["Curieux des nouvelles technologies", "Intéressé par la logistique", "Sans préférence"]),
+    ("6. Travailler en équipe pour développer une application vous semble :", ["Motivant", "Stressant", "Neutre"]),
+    ("7. Les langages de programmation sont pour vous :", ["Passionnants", "Compliqués", "Ennuyeux"]),
+    ("8. Vous aimez résoudre des problèmes :", ["Logiques", "Pratiques", "Aucun des deux"]),
+    ("9. L'innovation technologique vous :", ["Inspire", "Ne vous intéresse pas", "Vous laisse neutre"]),
+    ("10. Vous préférez travailler :", ["Sur ordinateur", "Avec des outils manuels", "Sans préférence"]),
+    ("11. Gérer des bases de données vous semble :", ["Utile", "Fastidieux", "Inutile"]),
+    ("12. Vous aimez participer à des projets :", ["Collaboratifs", "Individuels", "Je ne sais pas"]),
+    ("13. Développer des algorithmes vous paraît :", ["Amusant", "Difficile", "Inutile"]),
+    ("14. Vous vous sentez à l'aise avec :", ["Les logiciels", "Le matériel", "Aucun des deux"]),
+    ("15. Vous aimez explorer :", ["Les nouvelles technologies", "Les techniques traditionnelles", "Je ne sais pas"]),
+    ("16. Vous préférez :", ["Créer des solutions logicielles", "Utiliser des outils existants", "Aucun des deux"]),
+    ("17. Vous êtes motivé par :", ["La résolution de problèmes", "L'organisation", "Autre"]),
+    ("18. Apprendre de nouveaux langages de programmation vous semble :", ["Excitant", "Stressant", "Inutile"]),
+    ("19. Travailler sur des projets innovants vous :", ["Passionne", "Laisse indifférent", "Ennuie"]),
+    ("20. Être à jour sur les dernières technologies vous semble :", ["Essentiel", "Optionnel", "Inutile"]),
+]
 
-q2 = st.radio("2. Vous préférez :", ["Résoudre des problèmes de code", "Assembler des matériels", "Je ne sais pas"])
-if q2 == "Résoudre des problèmes de code":
-    score += 1
-
-q3 = st.radio("3. Les bases de données vous semblent :", ["Fascinantes", "Compliquées", "Inintéressantes"])
-if q3 == "Fascinantes":
-    score += 1
+for i, (question, options) in enumerate(questions):
+    answer = st.radio(question, options, key=i)
+    if answer in ["Créer des sites web et des applications", "Résoudre des problèmes de code", "Fascinantes", "Pratiquant avec des projets", "Curieux des nouvelles technologies", "Motivant", "Passionnants", "Logiques", "Inspire", "Sur ordinateur", "Utile", "Collaboratifs", "Amusant", "Les logiciels", "Les nouvelles technologies", "Créer des solutions logicielles", "La résolution de problèmes", "Excitant", "Passionne", "Essentiel"]:
+        score += 1
 
 if st.button("Voir les résultats"):
-    if score >= 2:
+    if score >= 15:
         st.success("Vous avez un profil idéal pour l'option SLAM !")
+    elif score >= 10:
+        st.info("Vous pourriez bien réussir dans l'option SLAM, avec un peu d'investissement !")
     else:
         st.warning("L'option SLAM pourrait être un défi intéressant pour vous, mais explorez aussi d'autres options.")
 
